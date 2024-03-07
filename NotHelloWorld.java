cb36db5e2bde79e010aeb043902f7ce5cd7f6e61
Daniel Andreas Wang <gamesman36@outlook.com>
2024-03-07 19:57:52 +0100

    First commit

---
+++ NotHelloWorld.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class NotHelloWorld {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            var frame = new NotHelloWorldFrame();
+            frame.setTitle("NotHelloWorld");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
---
+++ NotHelloWorldFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class NotHelloWorldFrame extends JFrame {
+    public NotHelloWorldFrame() {
+        add(new NotHelloWorldComponent());
+        pack();
+    }
+}
